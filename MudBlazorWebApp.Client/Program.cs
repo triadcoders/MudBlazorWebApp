eca34277902d003d4eb0bec9be0a2a2c3cdc1b10
triadcoders <chad27295@gmail.com>
2024-03-14 07:27:24 -0400

    Rendermode Auto global and dark mode and navbar collapse working

--- MudBlazorWebApp.Client/Program.cs
+++ MudBlazorWebApp.Client/Program.cs
@@ -7,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //builder.Services.AddMudServices();
 
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
 builder.Services.AddSingleton<IMessageController, ClientMessageController>();
 
 builder.Services.AddMudServices(config =>
